e1704158768ce67a17bb4ab971a285df8f856029
Marie Loeffer <mloeffer@z2r2p3.42lyon.fr>
2025-02-18 19:58:34 +0100

    add : more personalised error messages

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mloeffer <mloeffer@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/01/16 08:27:01 by mjzlak            #+#    #+#              #
-#    Updated: 2025/02/13 15:49:50 by mloeffer         ###   ########.fr        #
+#    Updated: 2025/02/18 17:16:35 by mloeffer         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,9 @@ SRCS = main.c \
 	   error_handling.c \
 	   utils/free.c \
 	   utils/path_checker.c \
-	   utils/init_lst.c
+	   utils/init_lst.c \
+	   utils/close.c \
+	   utils/do_and_return.c
 
 OBJS = $(SRCS:.c=.o)
 
--- include/pipex.h
+++ include/pipex.h
@@ -27,24 +27,29 @@ typedef struct s_cmd
 	struct s_cmd	*next;
 } t_cmd;
 
+// main.c
+int		main(int argc, char **argv, char **env);
+
 // error_handling.c
 int			file_checker(char *file1, char *file2);
 int			command_checker(char **av, char **env, int ac, int i);
 int			error_handling(int ac, char **av, char **env);
 
-// utils/free.c
-void	free_array(char **array);
-int		free_array_and_return_zero(char **array);
-void	free_cmd_lst(t_cmd *lst);
-t_cmd	*free_lst_and_return_null(t_cmd *lst);
 //	utils/init_lst.c
 t_cmd	*init_lst(char **argv, int argc, int i);
 // utils/path_checker.c
 int		is_a_valid_path(char **cmds, char **path, int i);
 char	**find_path(char **env, char *path, int len_of_path);
 char	*get_absolute_path(char *cmd, char **env);
-
-// main.c
-int		main(int argc, char **argv, char **env);
+// utils/free.c
+void	free_array(char **array);
+void	free_cmd_lst(t_cmd *lst);
+// utils/close.c
+void	close_all_opened_files(int *files, int *pipe_files, char *error);
+// utils/do_and_return.c
+int		free_array_and_return_zero(char **array, int return_value);
+t_cmd	*free_lst_and_return_null(t_cmd *lst);
+int		close_and_return(int *files, int *pipe_files, int return_value, char *error);
+int		perror_and_return(char *error, int return_value);
 
 #endif

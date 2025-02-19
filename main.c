550e0778b59c5f82d60d4450eaf7afc386002c34
Marie Loeffer <mloeffer@z4r11p4.42lyon.fr>
2025-02-19 18:00:54 +0100

    fix : error from excuting commands with execve

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 
 #include "include/pipex.h"
 
+// Execute the commands in the linked list
 static int	exec_commands(t_cmd *cmd, char **env)
 {
 	char	*absolute_path;
@@ -27,7 +28,8 @@ static int	exec_commands(t_cmd *cmd, char **env)
 	return (0);
 }
 
-static int	fork_process(t_cmd *line, char **env, int *fd, int *pipe_fd)
+// Fork a process, redirect the input and output and execute the command
+static int	fork_process(t_cmd *line, char **env, int fd, int pipe_fd)
 {
 	int	pid;
 
@@ -36,55 +38,40 @@ static int	fork_process(t_cmd *line, char **env, int *fd, int *pipe_fd)
 		return (perror_and_return("fork_process: fork failed !", 0));
 	if (pid == 0)
 	{
-		if (dup2(fd[0], STDIN_FILENO) == -1)
+		if (dup2(fd, STDIN_FILENO) == -1 || dup2(pipe_fd, STDOUT_FILENO) == -1)
 			return (perror_and_return("fork_process: dup2 failed !", 0));
-		if (pipe_fd)
-		{
-			if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-				return (perror_and_return("fork_process: dup2 failed !", 0));
-		}
-		else
-		{
-			if (dup2(fd[1], STDOUT_FILENO) == -1)
-				return (perror_and_return("fork_process: dup2 failed !", 0));
-		}
-		close_all_opened_files(fd, pipe_fd, NULL);
+		close(fd);
+		close(pipe_fd);
 		if (!exec_commands(line, env))
 			return (0);
 	}
-	//waitpid(pid, NULL, 0);
-	while (wait(NULL) > 0)
-		;
 	return (1);
 }
 
-static int	pipex_launcher(t_cmd *line, char **env, int nb_of_cmd)
+// Open the input file, create pipes and call fork_process for each command
+static int	pipex_launcher(t_cmd *line, char **env)
 {
-	t_cmd	*last;
 	int		fd[2];
-	int		pipe_fd[nb_of_cmd];
+	int		pipe_fd[2];
 
-	last = line;
-	while (last->next)
-		last = last->next;
-	if (!line || !line->infile[0] || !last->outfile[0])
+	if (!line || !line->infile[0])
 		return (0);
 	fd[0] = open(line->infile, O_RDONLY);
 	if (fd[0] < 0)
 		return (close_and_return(fd, pipe_fd, 0, "pipex_launcher : open failed ! :"));
-	fd[1] = open(last->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd[1] < 0)
-		return (close_and_return(fd, pipe_fd, 0, "pipex_launcher : open failed ! :"));
-	while (nb_of_cmd > 2)
+	while (line->next)
 	{
 		if (pipe(pipe_fd) == -1)
 			return (close_and_return(fd, pipe_fd, 0, "pipex_launcher : pipe failed ! :"));
-		if (!fork_process(line, env, fd, pipe_fd))
+		if (!fork_process(line, env, fd[0], pipe_fd[1]))
 			return (close_and_return(fd, pipe_fd, 0, NULL));
+		fd[0] = pipe_fd[0];
 		line = line->next;
-		nb_of_cmd--;
 	}
-	if (!fork_process(line, env, fd, NULL))
+	fd[1] = open(line->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd[1] < 0)
+		return (close_and_return(fd, pipe_fd, 0, "pipex_launcher : open failed ! :"));
+	if (!fork_process(line, env, fd[0], fd[1]))
 		return (close_and_return(fd, pipe_fd, 0, NULL));
 	return (close_and_return(fd, pipe_fd, 1, NULL));
 }
@@ -98,7 +85,7 @@ int	main(int argc, char **argv, char **env)
 	line = init_lst(argv, argc, 2);
 	if (!line)
 		return (-1);
-	if (!pipex_launcher(line, env, argc - 3))
+	if (!pipex_launcher(line, env))
 		return (-1);
 	free_cmd_lst(line);
 	return (0);
